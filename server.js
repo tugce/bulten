cfe8d801e82be0e3a58e95e8c19583fcd8103307
Tugce Sirin <ztugcesirin@gmail.com>
2016-01-28 00:02:55 +0200

    heroku deployment v2

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.get('*', function(req, res) {
         res.sendfile('./public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
     });
     // listen (start app with node server.js) ======================================
-    app.listen(8081);
+    //app.listen(8081);
     console.log("App listening on port 8081");
 
 
